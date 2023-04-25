19089cf9e194e4fcb847effc0422bc21fce95983
Rafał <rafal.jenda94@gmail.com>
2023-04-25 19:40:51 +0200

    added function which starts ball animation

--- js.js
+++ js.js
@@ -3,5 +3,26 @@ const inputQuestion = document.querySelector('input')
 const answer = document.querySelector('p.answer')
 const error = document.querySelector('p.error')
 
-const randomAnswers =["Tak!", "Nie!", "To nie najlepszy pomysł", "Chyba nie", "Raczej tak", "Poważnie się zastanów!", "Nigdy!!!", "Śmiało!", "Nie powinienieś tego robić"]
+const randomAnswers = [
+	'Tak!',
+	'Nie!',
+	'To nie najlepszy pomysł',
+	'Chyba nie',
+	'Raczej tak',
+	'Poważnie się zastanów!',
+	'Nigdy!!!',
+	'Śmiało!',
+	'Nie powinienieś tego robić',
+]
 
+const shakeAnimation = () => {
+	ballImg.classList.add('shake-animation')
+
+	const reset = () => {
+		ballImg.classList.remove('shake-animation')
+	}
+
+	setTimeout(reset, 1200)
+}
+
+ballImg.addEventListener('click', shakeAnimation)
