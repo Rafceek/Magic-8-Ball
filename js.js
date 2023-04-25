ea7b297d4435ef13a2a85393523f87bfa1f6c979
Rafał <rafal.jenda94@gmail.com>
2023-04-25 19:31:40 +0200

    downloaded needed elements and added array with random answers

---
+++ js.js
@@ -0,0 +1,7 @@
+const ballImg = document.querySelector('.ball-img img')
+const inputQuestion = document.querySelector('input')
+const answer = document.querySelector('p.answer')
+const error = document.querySelector('p.error')
+
+const randomAnswers =["Tak!", "Nie!", "To nie najlepszy pomysł", "Chyba nie", "Raczej tak", "Poważnie się zastanów!", "Nigdy!!!", "Śmiało!", "Nie powinienieś tego robić"]
+
